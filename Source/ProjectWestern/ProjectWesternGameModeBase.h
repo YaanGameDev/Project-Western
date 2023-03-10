7203ea2bba795947ac693b6f97c62b6156add2f5
Yaan - Aquiris <yan.silva@aquiris.com.br>
2023-03-10 11:35:40 -0300

    All Files project create

---
+++ Source/ProjectWestern/ProjectWesternGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectWesternGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTWESTERN_API AProjectWesternGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
