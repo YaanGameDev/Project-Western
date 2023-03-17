a603ecacd8c5c73c34b205e052719468b8873483
Yaan - Aquiris <yan.silva@aquiris.com.br>
2023-03-17 12:08:06 -0300

    Deleted Animation Jump and State Machine

--- Source/ProjectWestern/Private/CharacterRunner.cpp
+++ Source/ProjectWestern/Private/CharacterRunner.cpp
@@ -38,64 +38,12 @@ void ACharacterRunner::Tick(float DeltaTime)
 void ACharacterRunner::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	InputComponent->BindAction("Jump", IE_Pressed, this, &ACharacterRunner::PlayerJump);
-	InputComponent->BindAction("Jump", IE_Released, this, &ACharacterRunner::StopPlayerJump);
 
 }
 
 void ACharacterRunner::RunAutomatic()
 {
 	AddMovementInput(GetActorForwardVector() * RunSpeedPlayer);
-	currentPlayerState = EPlayerState::STATE_Run;
-}
-
-void ACharacterRunner::PlayerJump()
-{
-	currentPlayerState = EPlayerState::STATE_Jump;
-
-	GetCharacterMovement()->AirControl = 20.f;
-	GetCharacterMovement()->JumpZVelocity = 425.f;
-	GetCharacterMovement()->GravityScale = 1.5f;
-	
-
-}
-
-void ACharacterRunner::StopPlayerJump()
-{
-	IsJumping = false;
-}
-
-void ACharacterRunner::SetNewPlayerState(EPlayerState newState)
-{
-	PreviousPlayerState = currentPlayerState;
-	currentPlayerState = newState;
-}
-
-EPlayerState ACharacterRunner::GetPlayerState() const
-{
-	return currentPlayerState;
-}
-
-void ACharacterRunner::HandleStateEnd()
-{
-	switch (PreviousPlayerState)
-	{
-		case EPlayerState::STATE_Run:
-		{
-			break;
-		}
-		case EPlayerState::STATE_Jump:
-		{
-			break;
-		}	
-	}
-}
-
-void ACharacterRunner::HandleStateEntered()
-{
-	{
-
-	}
 }
 
 
